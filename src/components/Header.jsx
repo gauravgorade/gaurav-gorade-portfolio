4202a41726f8a4749d04ab538d7de7497a1cd566
Gaurav Gorade <gauravgorade2929@gmail.com>
2024-03-10 01:04:12 +0530

    Header Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,28 +8,28 @@ function Header() {
 
     const [openMenu, setopenMenu] = useState(false);
 
- 
+
 
     return (
         <header className={(openMenu) ? "header active" : "header"}>
             <div className="header-wraper">
-            <Link to="/"><p className='logo'> GAURAV GORADE  </p></Link>
-            <nav className="nav">
-                <ul className="nav-list">
-                    <li><Link to="/" className="nav-link">Home</Link></li>
-                    <li><Link to="/skills" className="nav-link">Skills</Link></li>
-                    <li> <Link to="/projects" className="nav-link">Projects</Link></li>
-                    <li> <Link to="/resume" className="nav-link">Resume</Link></li>
-                    <li><Link to="/contact" className="nav-link">Contact</Link></li>
-                </ul>
-            </nav>
-            <div className="mobile-nav-btn">
-                <IoMenuOutline name="menu-outline" onClick={() => setopenMenu(true)} className="mobile-nav-icon" />
-                <IoCloseOutline name="close-outline" onClick={() => setopenMenu(false)} className="mobile-nav-icon" />
-            </div>
+                <Link to="/"><p className='logo'> GAURAV GORADE  </p></Link>
+                <nav className="nav">
+                    <ul className="nav-list">
+                        <li><Link onClick={() => setopenMenu(false)} to="/" className="nav-link">Home</Link></li>
+                        <li><Link onClick={() => setopenMenu(false)} to="/skills" className="nav-link">Skills</Link></li>
+                        <li> <Link onClick={() => setopenMenu(false)} to="/projects" className="nav-link">Projects</Link></li>
+                        <li> <Link onClick={() => setopenMenu(false)} to="/resume" className="nav-link">Resume</Link></li>
+                        <li><Link onClick={() => setopenMenu(false)} to="/contact" className="nav-link">Contact</Link></li>
+                    </ul>
+                </nav>
+                <div className="mobile-nav-btn">
+                    <IoMenuOutline name="menu-outline" onClick={() => setopenMenu(true)} className="mobile-nav-icon" />
+                    <IoCloseOutline name="close-outline" onClick={() => setopenMenu(false)} className="mobile-nav-icon" />
+                </div>
 
             </div>
-            
+
         </header>
 
     );
--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -6,7 +6,7 @@ function Page404() {
   return (
     <>
     
-      <h1>Error 404</h1>
+      <h1>Error 404: Page not found</h1>
       
     </>
   );
